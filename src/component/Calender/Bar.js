3cd0b41b1786fa64954a1969542f8d989c6b7343
gwak <jaewoo010207@gmail.com>
2021-11-20 23:37:31 +0900

    buttons style update

--- src/component/Calender/Bar.js
+++ src/component/Calender/Bar.js
@@ -1,27 +1,28 @@
 import React from "react";
 import './calenderTab.css';
-import NextBtn from './nextBtn.jpg';
-import previousBtn from './previousBtn.jpg';
+import NextBtn from './right.png';
+import previousBtn from './left.png';
+
 import { useState, useEffect } from 'react'
 
 
 const Bar = () => {
     // 날짜 조작
     const [presentDay, setPresentDay] = useState({
-        year:0,
-        momth:0,
-        day:0
-    }) 
+        year: 0,
+        momth: 0,
+        day: 0
+    })
     const [today, setToday] = useState('') // 처음 랜더링
-    
-    const setDay = () =>{
+
+    const setDay = () => {
         const date = new Date();
         const year = date.getFullYear();
         const month = ("0" + (1 + date.getMonth())).slice(-2);
         const day = ("0" + date.getDate()).slice(-2);
         // setPresentDay({year : year, month : month, day : day});
         setPresentDay((prevState) => {
-            return { ...prevState, year : year, momth : month, day: day}
+            return { ...prevState, year: year, momth: month, day: day }
         })
         setToday(`${year}년 ${month}월 ${day}일`)
         console.log('gettoday', today)
@@ -45,11 +46,11 @@ const Bar = () => {
         console.log('yymmdd', yy, mm, dd);
         // console.log(`${yy}-${mm}-${dd+1}`)   
         // const date = new Date(yy, mm, dd);
-        const now = new Date(yy, mm-1, dd);
+        const now = new Date(yy, mm - 1, dd);
         console.log('now', now);
         var tomorrow = new Date(now.setDate(now.getDate() + 1));	// 내일
         console.log("내일 : ", tomorrow);
-        
+
         const year = tomorrow.getFullYear();
         const month = tomorrow.getMonth() + 1;
         const day = tomorrow.getDate();
@@ -60,7 +61,7 @@ const Bar = () => {
         // const yymmdd = [year, month, day];
         setToday(`${year}년 ${month}월 ${day}일`)
         setPresentDay((prevState) => {
-            return { ...prevState, year : year, momth : month, day : day}
+            return { ...prevState, year: year, momth: month, day: day }
         });
         // console.log(presentDay);
         // console.log(yymmdd);
@@ -73,11 +74,11 @@ const Bar = () => {
         console.log('yymmdd', yy, mm, dd);
         // console.log(`${yy}-${mm}-${dd+1}`)   
         // const date = new Date(yy, mm, dd);
-        const now = new Date(yy, mm-1, dd);
+        const now = new Date(yy, mm - 1, dd);
         console.log('now', now);
         var yesterday = new Date(now.setDate(now.getDate() - 1));	// 내일
         console.log("내일 : ", yesterday);
-        
+
         const year = yesterday.getFullYear();
         const month = yesterday.getMonth() + 1;
         const day = yesterday.getDate();
@@ -88,7 +89,7 @@ const Bar = () => {
         // const yymmdd = [year, month, day];
         setToday(`${year}년 ${month}월 ${day}일`)
         setPresentDay((prevState) => {
-            return { ...prevState, year : year, momth : month, day : day}
+            return { ...prevState, year: year, momth: month, day: day }
         });
         // console.log(presentDay);
         // console.log(yymmdd);
@@ -112,24 +113,25 @@ const Bar = () => {
         <div class="calender-bar">
             {console.log('rendering')}
             <div class="present">
-            {/* {console.log(presentDay, today)}                
-            {presentDay.year} */}
-            {today}
+                {/* {presentDay.day} */}
+                {today}
             </div>
             <div class="content-date">
-                <div onClick ={movePrevious} class="button previous">
+                {/* <button  onClick={movePrevious} class="ico_arrow button" type="button">클릭</button> */}
+                <div onClick={movePrevious} class="button previous">
                     <img src={previousBtn} alt="어제" />
                 </div>
                 <div class="day yesterday">
-
+                    {Number(presentDay.day) - 1}
                 </div>
                 <div class="day today">
-
+                    {presentDay.day}
                 </div>
                 <div class="day tomorrow">
+                    {Number(presentDay.day) + 1}
 
                 </div>
-                <div onClick ={moveNext} class="button next">
+                <div onClick={moveNext} class="button next">
                     <img src={NextBtn} alt="내일" />
                 </div>
             </div>
